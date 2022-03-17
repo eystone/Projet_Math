e5c68581dade4b6a5edcae508f79bbfd1d5c6d28
thomas noe <thomasnoe13@hotmail.fr>
2022-03-17 14:18:37 +0100

    Implements Dq and Eq files

--- Projet_Math.cpp
+++ Projet_Math.cpp
@@ -10,37 +10,53 @@
 
 using namespace std;
 
-int main()
-{
+void readFiles(string DirName) {
+	string dirname = "./BildeKrarup/" + DirName + "/files.lst";
+
+	ifstream mydir;
+	mydir.open(dirname);
+	if (!mydir.is_open()) cerr << "Can't open file " << dirname << '\n';
+	else {
+		ifstream myfile;
+		string line = "", filename = "";
+
+		while (getline(mydir, line)) {
+			string fileLine = "";
+			filename = "./BildeKrarup/" + DirName + '/' + line;
+			cout << "Filename = " << filename << '\n';
+			myfile.open(filename);
+			if (!myfile.is_open()) {
+				cerr << "Can't open file " << filename << '\n';
+				return;
+			}
+
+			while (getline(myfile, fileLine)) {
+				//cout << fileLine;
+			}
+			myfile.close();
+		}
+		mydir.close();
+	}
+	return;
+}
+
+int main() {
 	//B, C, Dq, Eq
-	vector<string> vDir(2);
+	vector<string> vDir(4);
 	vDir[0] = "B";
 	vDir[1] = "C";
+	vDir[2] = "Dq";
+	vDir[3] = "Eq";
 
 	for (int i(0); i < vDir.size(); ++i) {
-		string dirname = "./BildeKrarup/" + vDir[i] + "/files.lst";
-		
-		ifstream mydir;
-		mydir.open(dirname);
-		if (!mydir.is_open()) cerr << "Can't open file " << dirname << '\n';
-		else {
-			ifstream myfile;
-			string line = "", filename ="";
-
-			while (getline(mydir, line)) {
-				string fileLine = "";
-				filename = "./BildeKrarup/" + vDir[i] + '/' + line;
-				cout << "Filename = " << filename << '\n';
-				myfile.open(filename);
-				if (!myfile.is_open()) cerr << "Can't open file " << filename << '\n';
-				
-				while (getline(myfile, fileLine)) {
-					cout << fileLine;
-				}
-				cout << '\n';
-				myfile.close();
+		if (vDir[i] == "Dq" || vDir[i] == "Eq") {
+			for (int j(1); j <= 10; ++j) {
+				string str = vDir[i] + "/" + to_string(j);
+				readFiles(str);
 			}
-			mydir.close();
+		}
+		else {
+			readFiles(vDir[i]);
 		}
 	}
 	return 0;
